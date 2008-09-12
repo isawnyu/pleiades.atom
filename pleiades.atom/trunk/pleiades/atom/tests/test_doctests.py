7c4c3a3c383401046daa3ed9b3a4cecaa80c4403
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2008-09-12 05:09:11 +0000

    New project

---
+++ pleiades.atom/trunk/pleiades/atom/tests/test_doctests.py
@@ -0,0 +1,52 @@
+import unittest
+from zope.testing import doctestunit
+from zope.component import testing
+from Testing import ZopeTestCase as ztc
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+import pleiades.atom
+from pleiades.atom.tests.base import PleiadesAtomFunctionalTestCase
+
+ptc.setupPloneSite()
+
+class TestCase(ptc.PloneTestCase):
+    class layer(PloneSite):
+        @classmethod
+        def setUp(cls):
+            fiveconfigure.debug_mode = True
+            zcml.load_config('configure.zcml', pleiades.atom)
+            fiveconfigure.debug_mode = False
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='zgeo.plone.kml',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='zgeo.plone.kml.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        ztc.FunctionalDocFileSuite(
+            'topic.txt', package='pleiades.atom.tests',
+            test_class=PleiadesAtomFunctionalTestCase
+            ),
+
+        ztc.FunctionalDocFileSuite(
+            'place.txt', package='pleiades.atom.tests',
+            test_class=PleiadesAtomFunctionalTestCase
+            ),
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
---
+++ pleiades.atom/trunk/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='pleiades.atom',
+      version=version,
+      description="Atom formatting of Pleiades entities",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='atom plone georss pleiades',
+      author='Sean Gillies',
+      author_email='sgillies@frii.com',
+      url='http://atlantides.org/trac/pleiades/pleiades.atom',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pleiades'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
