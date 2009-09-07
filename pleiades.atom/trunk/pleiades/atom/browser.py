fe5b6f960f2136bf1dade51cd394118190d11130
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2009-09-07 13:54:34 +0000

    Get with the times: places can have their own locations, even undetermined ones. Also use tag URIs.

--- pleiades.atom/trunk/pleiades/atom/browser.py
+++ pleiades.atom/trunk/pleiades/atom/browser.py
@@ -1,22 +1,60 @@
+from pleiades.capgrids import Grid
 from zgeo.atom.browser import SubscriptionFeed, LinkEntry, rfc3339
+from zgeo.plone.atom.browser import BrainLinkEntry, TopicSubscriptionFeed
+from zgeo.geographer.interfaces import IGeoreferenced
 
 
-class PleiadesEntry(LinkEntry):
-
+class Tagged(object):
+    
     @property
     def id(self):
-        return 'urn:uuid:%s' % self.context.UID()
-    
-class PlaceFeed(SubscriptionFeed):
+        year = self.context.CreationDate().split('-')[0]
+        return 'tag:atlantides.org,%s:pleiades/%s' % (year, self.context.UID())
 
+
+class TaggedBrain(object):
+    
+    @property
+    def id(self):
+        year = self.context.CreationDate.split('-')[0]
+        return 'tag:atlantides.org,%s:pleiades/%s' % (year, self.context.UID)
+
+
+class PleiadesEntry(Tagged, LinkEntry):
+    pass
+
+
+class PlaceFeed(Tagged, SubscriptionFeed):
+    
     @property
     def updated(self):
         return rfc3339(self.context.ModificationDate())
-
+    
     @property
     def entries(self):
-        x = self.context.getLocations()
-        if len(x) > 0:
+        x = list(self.context.getLocations())
+        dc_coverage = self.context.getLocation()
+        if len(x) > 0 and x[0].getGeometry():
             yield PleiadesEntry(self.context, self.request)
+        if len(x) == 0 or not x[0].getGeometry():
+            if dc_coverage.startswith('http://atlantides.org/capgrids'):
+                s = dc_coverage.rstrip('/')
+                mapid, gridsquare = s.split('/')[-2:]
+                grid = Grid(mapid, gridsquare)
+                e = PleiadesEntry(self.context, self.request)
+                e.geom = IGeoreferenced(grid)
+                yield e
         for item in self.context.getFeatures():
             yield PleiadesEntry(item, self.request)
+
+
+class TopicEntry(TaggedBrain, BrainLinkEntry):
+    pass
+
+class TopicFeed(Tagged, TopicSubscriptionFeed):
+    
+    @property
+    def entries(self):
+        for brain in self.context.queryCatalog():
+            yield TopicEntry(brain, self.request)
+    
